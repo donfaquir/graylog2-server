a7d14a701e1d5b1b0689e6b6e043ddcb759c5a82
Dennis Oelkers <dennis@graylog.com>
2020-01-30 10:59:40 +0100

    Removing rickshaw residues.

--- graylog2-web-interface/src/routing/AppFacade.jsx
+++ graylog2-web-interface/src/routing/AppFacade.jsx
@@ -11,7 +11,6 @@ import 'bootstrap/less/bootstrap.less';
 import 'opensans-npm-webfont';
 import 'stylesheets/bootstrap-submenus.less';
 import 'toastr/toastr.less';
-import 'rickshaw/rickshaw.css';
 import 'stylesheets/graylog2.less';
 
 const SessionStore = StoreProvider.getStore('Session');
